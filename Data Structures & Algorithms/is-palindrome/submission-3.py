2873489c6b3291f43df4c1a8f57eec5161b0cbc4
Jamie Hilliam <91168116+JamieHilliam@users.noreply.github.com>
2026-09-10 12:02:07 +1200

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Alphanum Stuff
+        alphanum_list = []
+        for current_char in s:
+            if current_char.isalnum():
+                alphanum_list.append(current_char)
+        joined_str = ''.join(alphanum_list)
+        joined_str = joined_str.lower()
+        joined_len = len(joined_str)
+        for i in range(joined_len):
+            if joined_str[i] != joined_str[joined_len - i -1]:
+                return False
+        return True
